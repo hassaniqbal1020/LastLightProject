83614dfa13819cfdd024bca3c2585ae5650a050f
hassaniqbal1020 <60104411+hassaniqbal1020@users.noreply.github.com>
2020-04-02 00:40:36 +0100

    Update to character design and bug fixes

--- Final Year Project 0.3/Assets/Scripts/GrapplingHook1.cs
+++ Final Year Project 0.3/Assets/Scripts/GrapplingHook1.cs
@@ -9,7 +9,7 @@ public class GrapplingHook1 : MonoBehaviour
 
     public Transform[] Hooks; // Array of all hooks positions within game
     public Transform HookActive; // Position of currently active hook
-
+    public Transform HookSpawnPos; //Spawn point of hook
     public LayerMask mask; // What Raycast can collide with
 
     public LineRenderer HookLine; // Render hook within game
@@ -21,6 +21,8 @@ public class GrapplingHook1 : MonoBehaviour
     Vector3 targetPos; // Target position for hook
 
     RaycastHit2D hit; // Raycast to detect hook
+
+    public Animator playerAnim; // Referance to player animation controller
 
     // Start is called before the first frame update
     void Start()
@@ -54,44 +56,47 @@ public class GrapplingHook1 : MonoBehaviour
 
             ActiveState = "Active"; // Active after input
             targetPos = HookActive.transform.position; // Connected target equals active hook position
-
             targetPos.z = 0; //Set Z to 0
 
-            hit = Physics2D.Raycast(transform.position, targetPos - transform.position, MaxDistance, mask); //Set Raycast origin and direction
+            hit = Physics2D.Raycast(HookSpawnPos.position, targetPos - HookSpawnPos.position, MaxDistance, mask); //Set Raycast origin and direction
 
             if (hit.collider != null && hit.collider.gameObject.GetComponent<Rigidbody2D>() != null) //Raycast collides with something, collides with gameObject Rigidbody2D
             {
 
                 PlayerJoint.enabled = true; //Active
                 PlayerJoint.connectedBody = hit.collider.gameObject.GetComponent<Rigidbody2D>(); //Set ConnectedBody to the Rigidbady2D hit by Raycast
-                PlayerJoint.distance = Vector2.Distance(transform.position, hit.point); //Set distance between player and Rigidbody2D
+                PlayerJoint.distance = Vector2.Distance(HookSpawnPos.position, hit.point); //Set distance between player and Rigidbody2D
                 PlayerJoint.connectedAnchor = hit.point - new Vector2(hit.collider.transform.position.x, hit.collider.transform.position.y); // Point connects to the edge of hook rather than the middle
 
-                // Enabling line renderer and setting position
+                // Enabling line renderer and setting position and playing animation
                 HookLine.enabled = true; 
-                HookLine.SetPosition(0, transform.position);
+                HookLine.SetPosition(0, HookSpawnPos.position);
                 HookLine.SetPosition(1, hit.point);
-
+                playerAnim.SetBool("isSwinging", true);
 
             }
+
+            
 
         }
 
         if (Input.GetButton("Xbox_Right_Bumper")) // Enabling line renderer and setting position
         {
-            HookLine.SetPosition(0, transform.position);
+            HookLine.SetPosition(0, HookSpawnPos.position);
+            
 
         }
 
-        if (Input.GetButtonUp("Xbox_Right_Bumper")) // Disabling line renderer and setting position
+        if (Input.GetButtonUp("Xbox_Right_Bumper")) // Disabling line renderer & animation and setting position
         {
             PlayerJoint.enabled = false;
             HookLine.enabled = false;
             ActiveState = "InActive";
+            playerAnim.SetBool("isSwinging", false);
 
         }
 
-        if (PlayerJoint.distance > 0.7f) // Set minimum distance for hook length to begin decreasing
+        if (PlayerJoint.distance > 1.3f) // Set minimum distance for hook length to begin decreasing
         {
             PlayerJoint.distance -= DecreaseDistance * Time.deltaTime;
 
@@ -102,7 +107,7 @@ public class GrapplingHook1 : MonoBehaviour
 
         }
 
-        Debug.DrawRay(transform.position, targetPos - transform.position); // Raycast visible within scene view
+        Debug.DrawRay(HookSpawnPos.position, targetPos - HookSpawnPos.position); // Raycast visible within scene view
 
 
     }
